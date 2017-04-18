accbd8c69ffda1251e020d3bf0ac503d9794713f
Martin Soemarsono <martinsoemarsono@hotmail.com>
2017-04-18 10:20:08 -0500

    Add Touch Me If You Dare File

--- app/src/main/java/com/example/pavilion/lookslikeblank/TouchMeIfYouDare.java
+++ app/src/main/java/com/example/pavilion/lookslikeblank/TouchMeIfYouDare.java
@@ -11,13 +11,5 @@ public class TouchMeIfYouDare extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_touch_me_if_you_dare);
-
-        // Get the Intent that started this activity and extract the string
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-
-        // Capture the layout's TextView and set the string as its text
-        TextView textView = (TextView) findViewById(R.id.textView2);
-        textView.setText(message);
     }
 }
