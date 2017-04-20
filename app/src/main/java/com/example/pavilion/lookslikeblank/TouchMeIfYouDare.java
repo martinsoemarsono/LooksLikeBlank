2ae85fd16cd0276afebbc4e28df558cefe8bc20f
Martin Soemarsono <martinsoemarsono@hotmail.com>
2017-04-19 19:04:00 -0500

    Added pictures and additional pages

--- app/src/main/java/com/example/pavilion/lookslikeblank/TouchMeIfYouDare.java
+++ app/src/main/java/com/example/pavilion/lookslikeblank/TouchMeIfYouDare.java
@@ -3,13 +3,22 @@ package com.example.pavilion.lookslikeblank;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class TouchMeIfYouDare extends AppCompatActivity {
+public static final String EXTRA_MESSAGE1 = "com.example.pavilion.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_touch_me_if_you_dare);
+    }
+
+    public void CheckAnswer(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, AnswerActivity.class);
+        startActivity(intent);
     }
 }
