7efdc584ce0f7bb0181ef2e50500eac86ec065a7
Martin Soemarsono <martinsoemarsono@hotmail.com>
2017-04-21 17:33:00 -0500

    Fixed AnswerActivity.java to show the user's answer and the actual answer (2)

--- app/src/main/java/com/example/pavilion/lookslikeblank/AnswerActivity.java
+++ app/src/main/java/com/example/pavilion/lookslikeblank/AnswerActivity.java
@@ -6,6 +6,13 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class AnswerActivity extends AppCompatActivity {
+    private static final String ifAMatch = "We See The Same Thing!";
+
+    private static final String AcceptableAnswer1 = "A Man";
+    private static final String AcceptableAnswer2 = "A Human";
+    private static final String AcceptableAnswer3 = "A Human Being";
+    private static final String AcceptableAnswer4 = "Human";
+    private static final String AcceptableAnswer5 = "Man";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +26,19 @@ public class AnswerActivity extends AppCompatActivity {
         // Capture the layout's TextView and set the string as its text
         TextView textView = (TextView) findViewById(R.id.textView3);
         textView.setText(message);
+
+        TextView textView1 = (TextView) findViewById(R.id.textView6);
+
+        if (message.toLowerCase().equals(AcceptableAnswer1.toLowerCase())
+         || (message.toLowerCase().equals(AcceptableAnswer2.toLowerCase()))
+         || (message.toLowerCase().equals(AcceptableAnswer3.toLowerCase()))
+         || (message.toLowerCase().equals(AcceptableAnswer4.toLowerCase()))
+         || (message.toLowerCase().equals(AcceptableAnswer5.toLowerCase()))) {
+            textView1.setText(ifAMatch);
+        } else {
+            textView1.setText(null);
+        }
     }
+
+
 }
